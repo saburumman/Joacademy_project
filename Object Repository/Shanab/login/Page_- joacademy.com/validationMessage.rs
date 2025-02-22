4fbd37f2e990ee754987e6f0a1dd50effbe49456
mohammad2002-max <mohjoac@gmail.com>
2025-02-22 15:14:15 +0300

    firts-try

---
+++ Object Repository/Shanab/login/Page_- joacademy.com/validationMessage.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>validationMessage</name>
+   <tag></tag>
+   <elementGuidId>bcfa9d62-d8cb-41a7-b1e9-9584a7454fac</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>IMAGE</key>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(text(),'الايميل المحدد غير صالح')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
